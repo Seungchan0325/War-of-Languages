518beb2fe46fc22c52bf6c5dd3f3e5a875fae63f
Seungchan0325 <imchan0325@gmail.com>
2023-08-30 01:43:41 +0900

    new button list

--- src/scenes/selection_scene.py
+++ src/scenes/selection_scene.py
@@ -1,11 +1,11 @@
 import pygame
-from pygame.sprite import DirtySprite
+from pygame.sprite import DirtySprite, LayeredDirty
 
 from game_config import GameConfig
 from system.event_handler import EventHandler
 from system.scenes import BaseScene, Scenes
-from system.control.button import Button
-from system.screen import Screen, RatioRect, create_rect
+from system.control.button import Button, ButtonList
+from system.screen import Screen, RatioRect
 from scenes.play_scene import PlayScene
 
 
@@ -33,12 +33,8 @@ class FriendButton(Button):
                  nickname: str,
                  ip: str,
                  port: str,
-                 y_ratio: float):
-        rect = create_rect(0.05, y_ratio, 0.4, 0.05)
-
-        super().__init__(rect)
-
-        self._y_ratio = y_ratio
+                 rect: RatioRect):
+        super().__init__(rect.to_pyrect())
 
         self._prev_is_on = self.is_on_mouse()
 
@@ -71,15 +67,6 @@ class FriendButton(Button):
         else:
             self.image = self._surface_not_on_mouse
 
-    def position_update(self, y_ratio: float):
-        if abs(self._y_ratio - y_ratio) < 1e-10:
-            return
-
-        screen = Screen.instance()
-        self.dirty = 1
-        self._y_ratio = y_ratio
-        self.rect.top = int(screen.height * y_ratio)
-
     def update(self):
         if self._prev_is_on and not self.is_on_mouse():
             self.dirty = 1
@@ -96,104 +83,21 @@ class FriendButton(Button):
         #     scenes.change_scene(PlayScene())
 
 
-# class ButtonList(DirtySprite):
+class FriendList(ButtonList):
 
-#     def __init__(self,
-#                  area: RatioRect,
-#                  button_size: RatioRect):
-#         super().__init__()
-
-#         self.area = area
-
-#         self.rect = self.area.to_pyrect()
-
-#         self._scroll_cnt = 0
-#         self._button_size = button_size
-
-#         self.buttons: list[Button] = []
-
-#     def _compute_position(self, index: int) -> RatioRect:
-#         x = self.area.x
-#         y = (index + self._scroll_cnt) * self._button_size.h + self.area.h
-#         w = self.area.y
-#         h = self._button_size.h
-#         return RatioRect(x, y, w, h)
-
-#     def add_button(self, button: Button):
-#         self.buttons.append(button)
-
-#     def update(self):
-#         event_handler = EventHandler.instance()
-#         if self.rect.collidepoint(event_handler.get_mouse_pos()):
-#             if event_handler.mouse_scroll < 0 and self._scroll_cnt < 0:
-#                 self._scroll_cnt += 1
-#             elif event_handler.mouse_scroll > 0 and self._scroll_cnt > -len(self.buttons) + 1:
-#                 self._scroll_cnt -= 1
-
-#         for i, button in enumerate(self.buttons):
-#             pos = self._compute_position(i)
-#             button.position_update(pos)
-
-
-class FriendList():
-
-    def __init__(self):
-        super().__init__()
-
-        self._count = 0
-        self.buttons: list[FriendButton] = []
-
-        self._y_offset = 0.1
-        self._scroll_cnt = 0
-
-        self.selected_idx: int = 0
-
-        # ratio
-        self._button_height = 0.05
-
-        self.rect = create_rect(0.05, self._y_offset, 0.4, 0.8)
-
-    def compute_y_ratio(self, index: int):
-        return (index + self._scroll_cnt) * self._button_height + self._y_offset
-
-    def add_button(self, nickname: str, ip: str, port: str) -> FriendButton:
-        y_ratio = self.compute_y_ratio(self._count)
-
-        button = FriendButton(
-            nickname,
-            ip,
-            port,
-            y_ratio
-        )
-
-        self._count += 1
-
-        self.buttons.append(button)
-        return button
+    def __init__(self, button_size: RatioRect):
+        super().__init__(RatioRect(0.05, 0.1, 0.4, 0.9), button_size)
+        self._selected_idx = 0
 
     def update(self):
-        event_handler = EventHandler.instance()
-        if self.rect.collidepoint(event_handler.get_mouse_pos()):
-            if event_handler.mouse_scroll < 0 and self._scroll_cnt < 0:
-                self._scroll_cnt += 1
-            elif event_handler.mouse_scroll > 0 and self._scroll_cnt > -self._count + 1:
-                self._scroll_cnt -= 1
-
-        for i, button in enumerate(self.buttons):
-            y_ratio = self.compute_y_ratio(i)
-            button.position_update(y_ratio)
-            if y_ratio < self._y_offset:
-                button.visible = False
-            else:
-                button.visible = True
-
-        self.selected_idx = -self._scroll_cnt
-
+        self._selected_idx = -self._scroll_cnt
         for i, button in enumerate(self.buttons):
             if button.is_up_clicked(pygame.BUTTON_LEFT):
-                self.selected_idx = i
+                self._is_scrolled = True
+                self._selected_idx = i
                 self._scroll_cnt = -i
-                break
+
+        super().update()
 
 
 class SelectionScene(BaseScene):
@@ -201,16 +105,18 @@ class SelectionScene(BaseScene):
     def __init__(self):
         super().__init__()
 
-        self.friend_list = FriendList()
+        button_size = RatioRect(0, 0, 0.4, 0.05)
+
+        self.friend_list = FriendList(button_size)
+        self.sprites.add(self.friend_list)
 
         config = GameConfig.instance()
         friends = read_csv(config.friends_file_path)
 
         # friend[0] is field name
         for nickname, ip, port in friends[1:]:
-            button = self.friend_list.add_button(nickname, ip, port)
-            self.sprites.add(button)
+            button = FriendButton(nickname, ip, port, button_size)
+            self.friend_list.add_button(button)
 
     def update(self):
-        self.friend_list.update()
         self.sprites.update()
--- src/system/control/button.py
+++ src/system/control/button.py
@@ -1,10 +1,11 @@
 from abc import ABC
 
+import pygame
 from pygame import Rect
-from pygame.sprite import DirtySprite
+from pygame.sprite import DirtySprite, Group
 
 from system.event_handler import EventHandler
-from system.screen import RatioRect
+from system.screen import RatioRect, Screen
 
 
 class Button(ABC, DirtySprite):
@@ -32,3 +33,74 @@ class Button(ABC, DirtySprite):
         event_handler = EventHandler.instance()
         mouse_down = event_handler.is_mouse_up[button]
         return mouse_down and self.is_on_mouse()
+
+
+class ButtonList(DirtySprite):
+
+    def __init__(self,
+                 area: RatioRect,
+                 button_size: RatioRect):
+        super().__init__()
+
+        self.area: RatioRect = area
+
+        self.rect: Rect = self.area.to_pyrect()
+
+        surface = pygame.Surface(self.rect.size)
+        self.image = surface
+
+        self._scroll_cnt = 0
+        self._button_size = button_size
+
+        self._draw = False
+
+        self.sprites: Group = Group()
+        self.buttons: list[Button] = []
+
+    def _compute_position(self, index: int) -> RatioRect:
+        x = self.area.x
+        y = (index + self._scroll_cnt) * self._button_size.h + self.area.y
+        w = self._button_size.w
+        h = self._button_size.h
+        return RatioRect(x, y, w, h)
+
+    def add_button(self, button: Button):
+        pos = self._compute_position(len(self.buttons))
+        button.update_position(pos)
+
+        self.sprites.add(button)
+        self.buttons.append(button)
+        self._draw = True
+        self._is_scrolled = False
+
+    def update(self):
+        event_handler = EventHandler.instance()
+        if self.rect.collidepoint(event_handler.get_mouse_pos()):
+            if event_handler.mouse_scroll < 0 and self._scroll_cnt < 0:
+                self._scroll_cnt += 1
+                self._is_scrolled = True
+            elif event_handler.mouse_scroll > 0 and self._scroll_cnt > -len(self.buttons) + 1:
+                self._scroll_cnt -= 1
+                self._is_scrolled = True
+
+        if self._is_scrolled:
+            for i, button in enumerate(self.buttons):
+                pos = self._compute_position(i)
+                button.update_position(pos)
+
+        self.sprites.update()
+
+        for sprite in self.sprites:
+            if sprite.dirty > 0:
+                self._draw = True
+                break
+
+        if self._draw:
+            self.dirty = 1
+            surface = pygame.Surface(Screen.instance().size)
+            surface.fill("red")
+            self.sprites.draw(surface)
+            self.image = surface.subsurface(self.area.to_pyrect())
+
+        self._draw = False
+        self._is_scrolled = False
